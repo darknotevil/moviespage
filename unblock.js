45bcb561ee1ef968197eb3ec6cd87f0d9b429606
darknotevil <44245421+darknotevil@users.noreply.github.com>
2026-05-29 23:08:26 +0300

    Update unblock.js

--- unblock.js
+++ unblock.js
@@ -2,6 +2,8 @@
 	'use strict';
 
 	//Lampa. LGBT plugin
-	window.lampa_settings.disable_features.lgbt=true;
+    window.lampa_settings.disable_features.dmca = true;
+    window.lampa_settings.disable_features.lgbt = true;
+    window.lampa_settings.disable_features.blacklist = true;
 
 })();
